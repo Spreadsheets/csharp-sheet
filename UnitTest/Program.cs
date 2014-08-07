4945bcbea397a7f0e9ebc2ab8e670c40d6df7232
Robert Plummer <robertleeplummerjr@gmail.com>
2014-08-07 15:58:50 -0400

    Remove unused files

--- UnitTest/Program.cs
+++ UnitTest/Program.cs
@@ -43,6 +43,21 @@ namespace UnitTest
             }
 
             Console.Write("Done!");
+
+
+            var spreadsheets = new Spreadsheets();
+            var spreadsheet = spreadsheets.AddSpreadsheet();
+
+            var row = spreadsheet.AddRow();
+
+            var cellA1 = row.AddCell("250");
+            var cellB1 = row.AddCell("250");
+            var cellC1 = row.AddCell("800 - (SUM(A1:B1) + 100)", true);
+
+            var cell = spreadsheet["C", 1];
+            var value = cell.UpdateValue();
+            Console.Write(value.ToDouble());
+            Console.Read();
         }
     }
 }
